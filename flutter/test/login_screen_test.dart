4e9b4b10de1fdea8650e701843eb9152e88eb1c2
Joshua Bromelow <JBromelow04@gmail.com>
2025-04-04 11:28:37 +0100

    Added home_page_test file

--- flutter/test/login_screen_test.dart
+++ flutter/test/login_screen_test.dart
@@ -1,6 +1,6 @@
-import 'package:fitness_app/frontend/states/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fitness_app/frontend/states/login_screen.dart';
 
 // Testing for the Login Screen
 void main() {
