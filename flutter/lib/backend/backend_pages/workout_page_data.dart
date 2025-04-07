b1991f6790b5a078cf94751a3ba3f3e975be68b5
AustenVines <up2124146@myport.ac.uk>
2025-04-07 16:04:42 +0100

    workout inserts inprogress

--- flutter/lib/backend/backend_pages/workout_page_data.dart
+++ flutter/lib/backend/backend_pages/workout_page_data.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'connect_database.dart';
 
 int user_id = 2;
@@ -62,12 +64,47 @@ WHERE
 ORDER BY 
     w.workout_date_time DESC;
 ''';
+String workoutTitle = "";
+int dateTime = 000000000000; // hour, minute, year, month, day
+bool public = false;
+String createWorkout = '''INSERT INTO workouts (user_ID, workout_title, workout_date_time, workout_public)
+VALUES ($user_id, $workoutTitle, $dateTime, $public)
+RETURNING workout_ID;''';
+
+int workoutId = 42; // This comes from the RETURNING workout_ID;
+int exerciseId = 1; // ID from the exercises table
+String activityName = "Squats";
+String activityType = "strength"; // Must match ENUM type
+String notes = "3 sets of 10 reps";
+int reps = 10;
+String activityTime = "2025-04-07 08:15:00"; // timestamp format
+String weightMetric = "kg"; // Must match ENUM
+int incline = 0;
+String distanceMetric = "km";
+int distance = 0;
+int elevation = 0;
+
+String insertActivity = '''
+INSERT INTO activities (
+  workout_ID, exercise_ID, activity_name, activity_type, activity_notes,
+  activity_reps, activity_time, activity_weight_metric, activity_incline,
+  activity_distance_metric, activity_distance, activity_elevation
+)
+VALUES (
+  $workoutId, $exerciseId, '$activityName', '$activityType', '$notes',
+  $reps, '$activityTime', '$weightMetric', $incline,
+  '$distanceMetric', $distance, $elevation
+);
+''';
+
+
 Future<void> main() async {
   try {
     await connection.open();
     print('Connected to PostgreSQL ✅');
 
     await readQuery(workoutListQuery);
+    await insertQuery(, dictionary)
   } catch (e) {
     print('Error connecting to PostgreSQL ❌: $e');
   } finally {
