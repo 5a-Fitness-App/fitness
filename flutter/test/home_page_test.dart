5761bec89edc50866bca4c3994a9f7fe52ce3b5d
Joshua Bromelow <JBromelow04@gmail.com>
2025-05-07 10:29:06 +0100

    updated home_page_test

--- flutter/test/home_page_test.dart
+++ flutter/test/home_page_test.dart
@@ -2,19 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fitness_app/frontend/states/home_page.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  //Verify the presence of all UI elements
+  Widget createWidgetUnderTest() {
+    return const ProviderScope(
+      child: MaterialApp(home: HomePage()),
+    );
+  }
+
+  // Verify the presence of all UI elements
   testWidgets('Home Page UI elements are displayed',
       (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(home: HomePage()));
+    await tester.pumpWidget(createWidgetUnderTest());
 
     // Give time for the UI to load
     await tester.pumpAndSettle();
 
     // Verify workout buttons are visible
     expect(find.text('Log Workout'), findsOneWidget);
-    expect(find.text('Start Workout'), findsOneWidget);
 
     // Verify streak elements are visible
     expect(find.text('Streak'), findsOneWidget);
@@ -28,31 +34,49 @@ void main() {
     expect(find.text('Recent Activity'), findsOneWidget);
 
     // Verify posts are visible, waiting for backend to be implemented
-    //expect();
+    expect(find.text('View Workout'), findsWidgets);
 
     // Verify comments and likes are visible
     expect(find.textContaining('❤️'), findsWidgets);
     expect(find.textContaining('💬'), findsWidgets);
 
-    // Verify view workout button is visible
-    expect(find.text('View Workout'), findsWidgets);
-
     // Verify view more workouts button is visible
-    expect(find.text('View More Workouts'), findsOneWidget);
+    //expect(find.text('View More Workouts'), findsOneWidget);
   });
 
-  // Verify that the Log Workout button takes the user to the log workout page, waiting for backend to be implemented before attempting
+  // Verify that the Log Workout button takes the user to the log workout page
   testWidgets('Log Workout button takes user to log_workout_page',
       (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(home: HomePage()));
+    await tester.pumpWidget(createWidgetUnderTest());
 
     // Give time for the UI to load
     await tester.pumpAndSettle();
 
-    // Currently checks if pressing the log workout button works, needs to eventually be changed to take the user to log workout
+    // Tap log workout button
     final logWorkoutButton = find.text('Log Workout');
     expect(logWorkoutButton, findsOneWidget);
     await tester.tap(logWorkoutButton);
     await tester.pump();
+
+    // Verify the user is taken to the log workout page
+    expect(find.text('Post'), findsOneWidget);
+  });
+
+  // Verify that the View Workout button takes the user to view the workout
+  testWidgets('View Workout button takes user to view the workout',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(createWidgetUnderTest());
+
+    // Give time for the UI to load
+    await tester.pumpAndSettle();
+
+    // Tap log workout button
+    final viewWorkoutButton = find.text('View Workout');
+    expect(viewWorkoutButton, findsOneWidget);
+    await tester.tap(viewWorkoutButton);
+    await tester.pump();
+
+    // Verify that the user is taken to a workout
+    expect(find.text('Comments'), findsOneWidget);
   });
 }
